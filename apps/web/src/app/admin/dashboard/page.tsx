19a4fa96d7649067d77f8ebef713d48e5c3d8b36
zawkey2412 <24.zawkey@gmail.com>
2024-08-25 23:58:00 +0700

    fix foldering

--- apps/web/src/app/admin/dashboard/page.tsx
+++ apps/web/src/app/admin/dashboard/page.tsx
@@ -3,7 +3,6 @@ import EventList from './_component/eventlist';
 import StatisticsOverview from './_component/staticoverview';
 import RecentTransactions from './_component/recent';
 import GraphVisualizations from './_component/visualizations';
-import DateRangePicker from './_component/date';
 
 const Dashboard: React.FC = () => {
     return (
@@ -13,10 +12,6 @@ const Dashboard: React.FC = () => {
             <section className="mb-12 bg-gray-700 p-6 rounded-lg shadow-md">
                 <h2 className="text-2xl font-semibold mb-4 text-white">Statistics Overview</h2>
                 <StatisticsOverview />
-            </section>
-
-            <section className="mb-12 bg-gray-700 p-6 rounded-lg shadow-md">
-                <DateRangePicker />
             </section>
             
             <section className="mb-12 bg-gray-700 p-6 rounded-lg shadow-md">
